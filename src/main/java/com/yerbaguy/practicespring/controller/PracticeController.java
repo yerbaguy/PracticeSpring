d8cd305d5e2948500c0b2c354fc60e29c964c534
root <root@localhost.localdomain>
2017-09-26 14:39:13 +0200

    New commit

---
+++ src/main/java/com/yerbaguy/practicespring/controller/PracticeController.java
@@ -0,0 +1,79 @@
+package com.yerbaguy.practicespring.controller;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import com.yerbaguy.practicespring.model.SayHallo;
+
+
+@Controller
+public class PracticeController  {
+
+	
+	@RequestMapping( value = {"/","/welcome"}, method = RequestMethod.GET)
+	//@RequestMapping( value = {"/","/welcome"})
+	//@Path("/welcome")
+	public ModelAndView halloWorld(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
+		
+		//ModelAndView model = new ModelAndView("welcome");
+		
+		SayHallo sayHallo = new SayHallo();
+		
+		sayHallo.setHallo("lkjasdf");
+		
+		//String getMessage = sayHallo.getHallo();
+		
+	//	model.addAttribute("message", sayHallo.getHallo());
+		
+		//String message="lkjasdlkf";
+		
+		//model.addObject("msg", "lkajsdflkjasd");
+		
+		//return "welcome";
+		return new ModelAndView("welcome", "asdfasd", new SayHallo());
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseBody
+	public String yesHalloWorld(@ModelAttribute("SayHallo") SayHallo sayHallo, ModelMap model) {
+		
+		
+		
+		//System.out.println(message);
+		sayHallo.setHallo("lkjasdf");
+		String message = sayHallo.getHallo();
+		
+		System.out.println(message);
+		
+		//String message = "lkjasdf";
+		
+		//Map<String,Object> map = new HashMap<String, Object>();
+		
+		//map.put("message", sayHallo.getHallo());
+		
+		model.addAttribute("asdfasdf", sayHallo.getHallo());
+		
+		//ModelAndView mav = new ModelAndView("welcome");
+		
+		//return new ModelAndView("welcome", "message", message);
+		return "welcome";
+	}
+	
+	
+}
---
+++ src/main/java/com/yerbaguy/practicespring/model/SayHallo.java
@@ -0,0 +1,16 @@
+package com.yerbaguy.practicespring.model;
+
+public class SayHallo {
+	
+	private String hallo;
+	
+	
+	public void setHallo(String hallo) {
+		this.hallo = hallo;
+	}
+	
+	public String getHallo() {
+		return this.hallo;
+	}
+
+}
