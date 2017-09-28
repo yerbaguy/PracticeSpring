c9c893f524064f5cf4885a547eebadbdd5d0040d
root <root@localhost.localdomain>
2017-09-28 05:20:36 +0200

    Another commit

--- src/main/java/com/yerbaguy/practicespring/controller/PracticeController.java
+++ src/main/java/com/yerbaguy/practicespring/controller/PracticeController.java
@@ -13,8 +13,6 @@ public class PracticeController {
 	@RequestMapping("/welcome")
 	public ModelAndView helloWorld() {
 	 
-	//String message = "<br><div style='text-align:center;'>"
-	//+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
 		
 	String message = "Hallo World";	
 		
